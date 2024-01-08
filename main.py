c6546fa00fe28df3c9e40ef716ce680c9396ac15
YUHEI <80562226+aspiratio@users.noreply.github.com>
2024-01-08 14:56:14 +0900

    Itemクラスとgetメソッドを作成

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from cruds import item as item_cruds
 
 
 app = FastAPI()
 
 
-@app.get("/")
-async def example():
-    return {"message": "Hello World!"}
+@app.get("/items")
+async def find_all():
+    return item_cruds.find_all()
+
+
+@app.get("/items/{id}")
+async def find_by_id(id: int):
+    return item_cruds.find_by_id(id)
