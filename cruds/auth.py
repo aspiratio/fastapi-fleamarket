5ddc14cd6a41c12b7dd786e27e8a2d3e1f5558b9
YUHEI <80562226+aspiratio@users.noreply.github.com>
2024-01-14 10:07:59 +0900

    パスワードのハッシュ化

--- cruds/auth.py
+++ cruds/auth.py
@@ -1,10 +1,22 @@
+import hashlib
+import base64
+import os
 from sqlalchemy.orm import Session
 from schemas import UserCreate
 from models import User
 
 
 def create_user(db: Session, user_create: UserCreate):
-    new_user = User(**user_create.model_dump())  # Userモデルを作成
+    salt = base64.b64encode(os.urandom(32))  # ランダムな文字列
+    hashed_password = hashlib.pbkdf2_hmac(
+        "sha256", user_create.password.encode(), salt, 1000
+    ).hex()  # パスワードにソルトを付け足した上でハッシュ化する
+
+    new_user = User(
+        username=user_create.username,
+        password=hashed_password,
+        salt=salt.decode(),
+    )  # Userモデルを作成
     db.add(new_user)
     db.commit()
 
